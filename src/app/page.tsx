b069ae1b19d75b4200c0ebddfca1c4471d07e76f
Taeuk Gwak <gwak2837@kakao.com>
2022-12-22 15:59:30 +0900

    Add resume, coopang, jayudam project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -228,6 +228,253 @@ export default function HomePage() {
           </div>
         </li>
       </ul>
+      <h2 className="text-3xl my-8">프로젝트</h2>
+      <div className="grid grid-cols-[2fr_1fr_3fr] gap-4 my-4">
+        <h4>이력서</h4>
+        <div>본인</div>
+        <div>2022년 12월 12일 ~ 현재</div>
+      </div>
+      <div className="flex gap-0.5 my-2">
+        <a
+          href="https://github.com/rmfpdlxmtidl/resume"
+          target="_blank"
+          rel="noreferrer"
+          className="hover:no-underline	p-1 hover:bg-slate-100 rounded-lg focus:no-underline"
+        >
+          <div className="flex gap-1 items-center">
+            <Image src="/images/github.svg" alt="github" width="16" height="16" />
+            <h5 className="underline-offset-4	underline text-black decoration-gray-300 ">resume</h5>
+          </div>
+        </a>
+        <a
+          href="https://gwak2837.vercel.app"
+          target="_blank"
+          rel="noreferrer"
+          className="hover:no-underline	p-1 hover:bg-slate-100 rounded-lg focus:no-underline"
+        >
+          <div className="flex gap-1 items-center">
+            <Image src="/images/vercel.png" alt="github" width="16" height="16" />
+            <h5 className="underline-offset-4	underline text-black decoration-gray-300 ">
+              gwak2837.vercel.app
+            </h5>
+          </div>
+        </a>
+      </div>
+      <div>
+        동기: Notion, Confluence, Google Docs 등 온라인 기반 문서 편집기는 PDF 변환 기능이 미흡해,
+        깔끔한 문서 인쇄 및 PDF 변환을 목적으로 프로젝트를 시작함
+      </div>
+      <div className="flex flex-wrap gap-1 my-2">
+        <a href="" rel="noreferrer" target="_blank">
+          <img
+            src="https://img.shields.io/badge/Next.js-000000?style=flat-square&logo=next.js&logoColor=white"
+            alt="next.js"
+          />
+        </a>
+        <a href="" rel="noreferrer" target="_blank">
+          <img
+            src="https://img.shields.io/badge/Tailwind-06B6D4?style=flat-square&logo=tailwindcss&logoColor=white"
+            alt="tailwind"
+          />
+        </a>
+      </div>
+      <ol>
+        <li>빠른 개발을 위해 Next.js와 TailwindCSS를 사용함</li>
+      </ol>
+      <div className="border w-full my-6" />
+      <div className="grid grid-cols-[2fr_1fr_3fr] gap-4 my-4">
+        <h4>쿠팡 가격 알리미</h4>
+        <div>본인</div>
+        <div>2022년 11월 22일 ~ 현재</div>
+      </div>
+      <div className="flex gap-0.5 my-2">
+        <a
+          href="https://github.com/rmfpdlxmtidl/coopang"
+          target="_blank"
+          rel="noreferrer"
+          className="hover:no-underline	p-1 hover:bg-slate-100 rounded-lg focus:no-underline"
+        >
+          <div className="flex gap-1 items-center">
+            <Image src="/images/github.svg" alt="github" width="16" height="16" />
+            <h5 className="underline-offset-4	underline text-black decoration-gray-300 ">coopang</h5>
+          </div>
+        </a>
+        <a
+          href="https://github.com/rmfpdlxmtidl/coopang-backend"
+          target="_blank"
+          rel="noreferrer"
+          className="hover:no-underline	p-1 hover:bg-slate-100 rounded-lg focus:no-underline"
+        >
+          <div className="flex gap-1 items-center">
+            <Image src="/images/github.svg" alt="github" width="16" height="16" />
+            <h5 className="underline-offset-4	underline text-black decoration-gray-300	">
+              coopang-backend
+            </h5>
+          </div>
+        </a>
+        <a
+          href="https://coopang.vercel.app"
+          target="_blank"
+          rel="noreferrer"
+          className="hover:no-underline	p-1 hover:bg-slate-100 rounded-lg focus:no-underline"
+        >
+          <div className="flex gap-1 items-center">
+            <Image src="/images/vercel.png" alt="github" width="16" height="16" />
+            <h5 className="underline-offset-4	underline text-black decoration-gray-300 		">
+              coopang.vercel.app
+            </h5>
+          </div>
+        </a>
+      </div>
+      <div>
+        동기: 시시각각 변하는 쿠팡의 가격을 추적하여 알림을 받기 위해 프로젝트를 시작함. 이후 이
+        기능을 쿠팡 파트너스의 제휴 URL과 연계해 수입을 얻을 예정임
+      </div>
+      <div className="flex flex-wrap gap-1 my-2">
+        <a href="" rel="noreferrer" target="_blank">
+          <img
+            src="https://img.shields.io/badge/Next.js-000000?style=flat-square&logo=next.js&logoColor=white"
+            alt="next.js"
+          />
+        </a>
+        <a href="" rel="noreferrer" target="_blank">
+          <img
+            src="https://img.shields.io/badge/Tailwind-06B6D4?style=flat-square&logo=tailwindcss&logoColor=white"
+            alt="tailwind"
+          />
+        </a>
+        <a href="" rel="noreferrer" target="_blank">
+          <img
+            src="https://img.shields.io/badge/React Query-FF4154?style=flat-square&logo=react-query&logoColor=white"
+            alt="react-query"
+          />
+        </a>
+        <a href="" rel="noreferrer" target="_blank">
+          <img
+            src="https://img.shields.io/badge/Fastify-000000?style=flat-square&logo=fastify&logoColor=white"
+            alt="fastify"
+          />
+        </a>
+      </div>
+      <ol>
+        <li>
+          Apollo server 사용하다가 Apollo는 서버에 HTTP2를 적용할 수 없어서 HTTP2를 지원하는
+          Fastify로 변경함. Fastify 출처인 벤치마크에도 Fastify가 Apollo보다 몇 배 빠르다고 해서
+          Fastify를 선택함
+        </li>
+        <li>GraphQL을 안 쓰게 돼서 Apoll client를 react query로 변경함</li>
+        <li>
+          Next.js 페이지 revalidation을 위해 Next.js api 기능을 활용함. 이때 lru-cache를 통해 요청
+          빈도를 제한함. 또한 font 로딩 속도 개선과 font 지연 로딩으로 인한 cumulative layout shift
+          방지를 위해 @next/font를 사용함
+        </li>
+        <li>
+          지금까지 Styled components를 사용했는데 Tailwind npm 주간 다운로드 수가 급격히 증가하기도
+          했고, Styled components를 사용하면 프로젝트 규모가 커질 수록 css 번들 크기가 선형으로
+          증가하니까, 큰 프로젝트에서 css 번들 크기를 줄이기 위해 Tailwind를 사용하려고 함. 그때를
+          위해 연습할 겸 Tailwind를 선택함
+        </li>
+        <li>
+          Push API를 웹워커로 직접 구현하다가 Push API 관련 UI 구현 및 UX 개선하는데 품이 많이
+          들어서 FlareLane을 선택함. 나중에 프로젝트 규모가 커져서 성능 및 비용 문제가 발생하면 다시
+          직접 웹워커로 구현할 예정임
+        </li>
+      </ol>
+      <div className="border w-full my-6" />
+      <div className="grid grid-cols-[2fr_1fr_3fr] gap-4 my-4">
+        <h4>자유담</h4>
+        <div>본인</div>
+        <div>2022년 6월 1일 ~ 10월 31일 (5개월)</div>
+      </div>
+      <div className="flex gap-0.5 my-2">
+        <a
+          href="https://github.com/rmfpdlxmtidl/jayudam"
+          target="_blank"
+          rel="noreferrer"
+          className="hover:no-underline	p-1 hover:bg-slate-100 rounded-lg focus:no-underline"
+        >
+          <div className="flex gap-1 items-center">
+            <Image src="/images/github.svg" alt="github" width="16" height="16" />
+            <h5 className="underline-offset-4	underline text-black decoration-gray-300 ">jayudam</h5>
+          </div>
+        </a>
+        <a
+          href="https://github.com/rmfpdlxmtidl/jayudam-backend"
+          target="_blank"
+          rel="noreferrer"
+          className="hover:no-underline	p-1 hover:bg-slate-100 rounded-lg focus:no-underline"
+        >
+          <div className="flex gap-1 items-center">
+            <Image src="/images/github.svg" alt="github" width="16" height="16" />
+            <h5 className="underline-offset-4	underline text-black decoration-gray-300 ">
+              jayudam-backend
+            </h5>
+          </div>
+        </a>
+        <a
+          href="https://jayudam.vercel.app"
+          target="_blank"
+          rel="noreferrer"
+          className="hover:no-underline	p-1 hover:bg-slate-100 rounded-lg focus:no-underline"
+        >
+          <div className="flex gap-1 items-center">
+            <Image src="/images/vercel.png" alt="github" width="16" height="16" />
+            <h5 className="underline-offset-4	underline text-black decoration-gray-300 ">
+              jayudam.vercel.app
+            </h5>
+          </div>
+        </a>
+      </div>
+      <div className="flex flex-wrap gap-1 my-2">
+        <a href="" rel="noreferrer" target="_blank">
+          <img
+            src="https://img.shields.io/badge/Apollo client-311C87?style=flat-square&logo=Apollo GraphQL&logoColor=white"
+            alt="react-query"
+          />
+        </a>
+        <a href="" rel="noreferrer" target="_blank">
+          <img
+            src="https://img.shields.io/badge/Recoil.js-3578e5?style=flat-square&logo=&logoColor=white"
+            alt="next.js"
+          />
+        </a>
+        <a href="" rel="noreferrer" target="_blank">
+          <img
+            src="https://img.shields.io/badge/React Form-EC5990?style=flat-square&logo=react-hook-form&logoColor=white"
+            alt="fastify"
+          />
+        </a>
+        <a href="" rel="noreferrer" target="_blank">
+          <img
+            src="https://img.shields.io/badge/Apollo server-311C87?style=flat-square&logo=Apollo GraphQL&logoColor=white"
+            alt="tailwind"
+          />
+        </a>
+      </div>
+      <ol>
+        <li>
+          네트워크 요청 및 응답 데이터와 input 입력값 데이터 관리를 제외한, React 전역 상태 관리를
+          위해 Recoil.js를 선택함. 네트워크 요청 및 응답 데이터는 Apollo client로 관리하고, input
+          입력값 데이터는 React form으로 관리함
+        </li>
+      </ol>
+      <Image
+        src="/images/jayudam.png"
+        alt="jayudam architecture"
+        width="1920"
+        height="1080"
+        className="border"
+      />
+      <div className="border w-full my-6" />
+
+      <h4 className="my-8">2022년 이전 프로젝트 목록</h4>
+      <a
+        href="https://gwak2837.notion.site/Resume-3fb93cf3019243fc8fd9103975f2872f"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Notion
+      </a>
       <h2 className="text-3xl my-8">기술</h2>
       <h4 className="my-2">Language</h4>
       <div className="flex gap-1">
@@ -459,7 +706,6 @@ export default function HomePage() {
           </a>
         </div>
       </div>
-
       <h4 className="my-2">Cloud</h4>
       <div className="grid gap-1">
         <div className="flex gap-1">
@@ -595,50 +841,40 @@ export default function HomePage() {
           </a>
         </div>
       </div>
-
       <h3 className="text-2xl my-4">React.js (Next.js)</h3>
       <dl>
-        <li>Recoil.js, Context API 등 전역 상태 관리 경험이 있습니다.</li>
-        <li>
-          React Query, Apollo client, useSWR 등 네트워크 요청 및 응답 데이터 관리 경험이 있습니다.
-        </li>
-        <li>Next.js 프레임워크 기반 SSR 개발 경험이 있습니다.</li>
+        <li>Recoil.js, Context API 기능으로 전역 상태를 관리함</li>
+        <li>React Query, Apollo client, useSWR 등 네트워크 요청 및 응답 데이터를 관리함</li>
+        <li>Next.js 프레임워크 기반 SSR를 개발함</li>
         <li>
           PWA를 적용해 모바일 접근성을 높이고, 브라우저 Notification/Push API로 사용자 재방문율을
-          향상했습니다.
+          향상함
         </li>
-        <li>Virtual DOM의 존재 의의와 언제 Reconciliation이 발생하는지 고민했습니다.</li>
+        <li>Virtual DOM의 존재 의의와 언제 Reconciliation이 발생하는지 고민함</li>
         <li>
-          함수 컴포넌트와 클래스 컴포넌트를 비교하며 여러 useEffect가 어떤 순서로 실행되는지
-          고민했습니다.
+          함수 컴포넌트와 클래스 컴포넌트를 비교하며 여러 useEffect가 어떤 순서로 실행되는지 고민함
         </li>
-        <li>
-          함수형 프로그래밍에서 나오는 클로저 개념을 활용해 useState를 간단하게 직접 구현했습니다.
-        </li>
-        <li>
-          메모이제이션 개념을 이용한 useMemo, useCallback을 활용해 컴포넌트 렌더링을 최적화했습니다.
-        </li>
+        <li>함수형 프로그래밍에서 나오는 클로저 개념을 활용해 useState를 간단하게 직접 구현함</li>
+        <li>메모이제이션 개념을 이용한 useMemo, useCallback을 활용해 컴포넌트 렌더링을 최적화함</li>
       </dl>
       <h3 className="text-2xl my-4">HTML · CSS</h3>
       <dl>
         <li>
-          W3C 웹 표준과 웹 접근성(a11y)을 지키려 노력하며 크로스 브라우징에 대응할 수 있습니다.
-          (IE11 제외)
+          W3C 웹 표준과 웹 접근성(a11y)을 지키려 노력하며 크로스 브라우징에 대응할 수 있음 (IE11
+          제외)
         </li>
-        <li>Semantic HTML, SCSS 문법에 익숙합니다.</li>
-        <li>keyframe, transition 등을 활용한 애니메이션 기법을 적용시킬 수 있습니다.</li>
-        <li>마우스 Drag&Drop 기능과 스크롤 이벤트를 활용한 애니메이션을 구현할 수 있습니다.</li>
-        <li>
-          Zeplin, Figma, Adobe XD와 같은 디자인 툴을 이용해 디자이너와 협업한 경험이 있습니다.
-        </li>
-        <li>순수 CSS를 이용해 Drawer, Modal, Carousel을 직접 구현한 적이 있습니다.</li>
+        <li>Semantic HTML, SCSS 문법에 익숙함</li>
+        <li>keyframe, transition 등을 활용한 애니메이션 구현 가능함</li>
+        <li>마우스 Drag&Drop 기능과 스크롤 이벤트를 활용한 애니메이션 구현 가능함</li>
+        <li>Figma, Zeplin, Adobe XD와 같은 디자인 툴을 이용해 디자이너와 협업함</li>
+        <li>순수 CSS를 이용해 Drawer, Modal, Carousel을 직접 구현 가능함</li>
       </dl>
       <h3 className="text-2xl my-4">JavaScript</h3>
       <dl>
-        <li>ES2022 까지의 문법 및 JavaScript 엔진에서 사용되는 비동기 개념에 익숙합니다.</li>
-        <li>TypeScript와 type 자동 생성 라이브러리를 사용해 개발 생산성을 높였습니다.</li>
-        <li>esbuild, Webpack 번들러를 사용해 Docker 이미지 크기를 줄였습니다.</li>
-        <li>Promise, async await, generator 문법을 사용해 비동기 작업을 구현했습니다.</li>
+        <li>ES2022 까지의 문법 및 JavaScript 엔진에서 사용되는 비동기 개념에 익숙함</li>
+        <li>TypeScript와 type 자동 생성 라이브러리를 사용해 개발 생산성을 높임</li>
+        <li>esbuild, Webpack 번들러를 사용해 Docker 이미지 크기를 줄임</li>
+        <li>Promise, async await, generator 문법을 사용해 비동기 작업을 구현 가능함</li>
         <li>
           <a
             href="https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API"
@@ -647,7 +883,7 @@ export default function HomePage() {
           >
             Intersection Observer API
           </a>
-          를 사용해 무한스크롤 페이지네이션을 구현했습니다.
+          를 사용해 무한스크롤 페이지네이션을 구현함
         </li>
         <li>
           브라우저의{' '}
@@ -682,116 +918,53 @@ export default function HomePage() {
           >
             Push API
           </a>
-          &nbsp;기능을 사용했습니다.
+          &nbsp;기능을 사용할 수 있음
         </li>
-        <li>웹소켓 또는 HTTP/2 Push 기능을 활용해 일대일 채팅 기능을 구현했습니다.</li>
+        <li>웹소켓 또는 HTTP/2 Push 기능을 활용해 일대일 채팅 기능을 구현함</li>
       </dl>
       <h3 className="text-2xl my-4">Backend</h3>
       <dl>
-        <li>Fastify, Next.js, Express.js, Koa.js 라이브러리로 REST API 서버를 개발했습니다.</li>
-        <li>Apollo Server를 사용해 GraphQL API 서버를 개발했습니다.</li>
-        <li>Jest를 사용한 유닛 테스트 자동화(CI) 경험이 있습니다.</li>
+        <li>Fastify, Next.js, Express.js, Koa.js 라이브러리로 REST API 서버를 개발함</li>
+        <li>Apollo Server를 사용해 GraphQL API 서버를 개발함</li>
+        <li>Jest를 사용한 유닛 테스트 자동화(CI) 경험이 있음</li>
+        <li>Dockerfile, compose.yaml 파일을 사용해 서버 인프라 설정, 컨테이너 관리를 자동화함</li>
+        <li>간단한 PostgreSQL 쿼리 빌더 및 ORM을 직접 구현함</li>
         <li>
-          Dockerfile, compose.yaml 파일을 사용해 서버 인프라 설정, 컨테이너 관리를 자동화했습니다.
-        </li>
-        <li>간단한 PostgreSQL 쿼리 빌더 및 ORM을 직접 구현했습니다.</li>
-        <li>
-          BFF 개념을 활용해 프론트엔드 개별 페이지에 최적화된 엔드포인트를 설계 · 구현할 수
-          있습니다.
+          BFF 개념을 활용해 프론트엔드 개별 페이지에 최적화된 엔드포인트를 설계 · 구현할 수 있음
         </li>
       </dl>
       <h3 className="text-2xl my-4">PostgreSQL</h3>
       <dl>
         <li>
-          외래키와 junction 테이블을 사용해 테이블 간 일대일, 일대다, 다대다 관계를 설정한 경험이
-          있습니다.
+          외래키와 junction 테이블을 사용해 테이블 간 일대일, 일대다, 다대다 관계를 설정할 수 있음
         </li>
-        <li>JOIN 키워드를 활용해 여러 테이블에서 데이터를 가져올 수 있습니다.</li>
-        <li>COPY 키워드를 사용해 데이터베이스 데이터를 CSV 파일로 관리한 경험이 있습니다.</li>
-        <li>트랜잭션으로 여러 SQL문을 하나의 논리적 단위로 관리할 수 있습니다.</li>
-        <li>함수, 프로시저, 트리거, PL/pgSQL을 사용해 SQL 요청 횟수를 줄인 경험이 있습니다.</li>
+        <li>JOIN 키워드를 활용해 여러 테이블에서 데이터를 가져올 수 있음</li>
+        <li>COPY 키워드를 사용해 데이터베이스 데이터를 CSV 파일로 관리할 수 있음</li>
+        <li>트랜잭션으로 여러 SQL문을 하나의 논리적 단위로 관리할 수 있음</li>
+        <li>함수, 프로시저, 트리거, PL/pgSQL을 사용해 SQL 요청 횟수를 줄일 수 있음</li>
         <li>
-          SQL 간 경쟁조건이 있을 때 의도된 결과를 얻기 위해 특정 테이블 또는 레코드를 잠글 수
-          있습니다.
+          SQL 간 경쟁조건이 있을 때 의도된 결과를 얻기 위해 특정 테이블 또는 레코드를 잠글 수 있음
         </li>
-        <li>Offset Pagination, Keyset Pagination 쿼리를 구현한 경험이 있습니다.</li>
+        <li>Offset Pagination, Keyset Pagination 쿼리를 구현할 수 있음</li>
       </dl>
       <h3 className="text-2xl my-4">Cloud</h3>
       <dl>
-        <li>
-          클라우드 가상 머신에 Docker 기반 SSL 전용 PostgreSQL 서버와 Redis 서버를 구축했습니다.
-        </li>
-        <li>
-          Cloud Build와 Cloud Run을 활용해 Docker 이미지 빌드 · 배포를 자동화(CI · CD) 했습니다.
-        </li>
-        <li>Cloud Function을 사용한 Slack 알림봇 개발 경험이 있습니다.</li>
-        <li>GCP VPC 네트워크에서 방화벽 규칙을 설정해 요청 IP 또는 포트 범위를 제한했습니다.</li>
-        <li>Cloud Storage에 이미지 파일을 동적으로 업로드하고 서비스 했습니다.</li>
-        <li>Cloud SQL 및 AWS RDS에 클라우드 DB 서버를 구축했습니다.</li>
-        <li>
-          AWS RDS, AWS EC2, AWS S3, Oracle Instance, 가비아 클라우드를 사용한 경험이 있습니다.
-        </li>
+        <li>클라우드 가상 머신에 Docker 기반 SSL 전용 PostgreSQL 서버와 Redis 서버를 구축함</li>
+        <li>Cloud Build와 Cloud Run을 활용해 Docker 이미지 빌드 · 배포를 자동화(CI · CD)함</li>
+        <li>Cloud Function을 사용한 Slack 알림봇 개발 가능함</li>
+        <li>GCP VPC 네트워크에서 방화벽 규칙을 설정해 요청 IP 또는 포트 범위를 제한함</li>
+        <li>Cloud Storage에 이미지 파일을 동적으로 업로드하고 서비스 했음</li>
+        <li>Cloud SQL 및 AWS RDS에 클라우드 DB 서버를 구축했음</li>
+        <li>AWS RDS, AWS EC2, AWS S3, Oracle Instance, 가비아 클라우드를 사용한 경험이 있음</li>
       </dl>
       <h3 className="text-2xl my-4">외부 API</h3>
       <dl>
-        <li>카카오 OAuth, Google OAuth, 비바톤 OAuth, 네이버 OAuth 등을 사용했습니다.</li>
-        <li>카카오페이, 토스페이를 사용해 결제 시스템을 자동화한 경험이 있습니다.</li>
-        <li>Google Analytics를 활용한 사이트 통계 분석 경험이 있습니다.</li>
-        <li>네이버지도 API를 활용해 지도를 사용한 경험이 있습니다.</li>
-        <li>
-          채널톡 API, Telegram API, SOLAPI를 활용해 사용자에게 알림용 메시지를 보낸 경험이 있습니다.
-        </li>
+        <li>카카오 OAuth, Google OAuth, 비바톤 OAuth, 네이버 OAuth 등과 연동 가능함</li>
+        <li>카카오페이, 토스페이를 사용해 결제 시스템을 자동화할 수 있음</li>
+        <li>Google Analytics를 활용한 사이트 통계 분석 경험이 있음</li>
+        <li>네이버지도 API를 활용해 지도 기능을 개발할 수 있음</li>
+        <li>채널톡 API, Telegram API, SOLAPI를 활용해 사용자에게 알림용 메시지를 보낼 수 있음</li>
       </dl>
-
-      <h2 className="text-3xl my-8">프로젝트</h2>
-      <div className="grid grid-cols-[2fr_1fr_3fr] gap-4 my-4">
-        <h4>쿠팡 가격 알리미</h4>
-        <div>1명</div>
-        <div>2022년 11월 22일 ~ 현재</div>
-      </div>
-      <div className="flex gap-1">
-        <div>(깃허브 저장소)</div>
-        <div>(홈페이지 링크)</div>
-      </div>
-      <div>
-        Apollo server 사용하다가 Apollo는 서버에 HTTP2를 적용할 수 없어서 HTTP2를 지원하는 Fastify로
-        변경
-      </div>
-      <div>Fastify 출처인 벤치마크에도 Fastify가 Apollo보다 몇 배 빠르다고 해서 Fastify 선택</div>
-      <div>GraphQL을 안 쓰게 돼서 Apoll client를 react query로 변경</div>
-      <div>
-        font 로딩 속도 개선과 font 변경으로 인한 cumulative layout shift 방지를 위해 next font 선택
-      </div>
-      <div>
-        nextjs 페이지 revalidation을 위해 nextjs api 기능 사용. 이때 lru-cache를 통해 요청 빈도를
-        제한함
-      </div>
-      <div>
-        지금까지 styled components 사용했는데 tailwind npm 주간 다운로드 수가 styled components를
-        넘기도 했고, 프로젝트 규모가 커질 수록 styled components 사용하면 css 번들 크기가 선형으로
-        증가하니까, 큰 프로젝트에서 css 번들 크기를 줄이기 위해 tailwindcss 사용하려고 함. 그때를
-        위해 연습할 겸 선택
-      </div>
-      <div>
-        웹워커에 push api 직접 구현하다가 push api 관련 ui 구현 및 ux 개선하기 지쳐서 flarelane
-        선택. 나중에 프로젝트 규모가 커져서 성능 및 비용 문제가 발생하면 직접 웹워커에서 push api
-        구현 예정
-      </div>
-      <div>---공통---</div>
-      <div>
-        네트워크 요청 및 응답 데이터와 input 입력값 데이터 관리를 제외한, React 전역 상태 관리를
-        위해 recoil 선택함. 네트워크 요청 및 응답 데이터 관리는 react query로, input 입력값 데이터
-        관리는 react form에서 함.
-      </div>
-      <div className="border w-full my-6" />
-      <h4 className="my-8">2022년 이전 프로젝트 목록</h4>
-      <a
-        href="https://gwak2837.notion.site/Resume-3fb93cf3019243fc8fd9103975f2872f"
-        target="_blank"
-        rel="noreferrer"
-      >
-        Notion
-      </a>
       <h2 className="text-3xl my-8">경험</h2>
       <ul>
         <li>
