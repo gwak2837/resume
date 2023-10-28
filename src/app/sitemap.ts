d40134273ef56ee10362c4dda912b9f210c236b6
Taeuk Gwak <gwak2837@kakao.com>
2023-10-28 14:21:21 +0900

    refactor: package 업그레이드

---
+++ src/app/sitemap.ts
@@ -0,0 +1,14 @@
+import { MetadataRoute } from 'next'
+
+import { CANONICAL_URL } from '../common/constants'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: CANONICAL_URL,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+  ]
+}
