5ef0e49b7f55730eeabbc85f45ab6d5acaa3bcd7
곽태욱 <gwak2837@gmail.com>
2024-09-26 16:21:54 +0900

    Update page.tsx

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -60,8 +60,8 @@ export default function HomePage({ params }: PageProps) {
             src="/images/profile.webp"
             className="w-[178px] aspect-square"
             alt="profile"
-            width={178}
-            height={178}
+            width={712}
+            height={712}
             priority
           />
           <ul className="flex flex-col gap-1">
@@ -219,8 +219,8 @@ export default function HomePage({ params }: PageProps) {
                   <Image
                     src="/images/ridi-icon.ico"
                     alt="ridi"
-                    width="282"
-                    height="282"
+                    width="80"
+                    height="80"
                     className="h-5 w-5 border rounded"
                   />
                   <h5 className="underline-offset-4 font-medium underline text-black decoration-gray-300 ">
@@ -374,8 +374,8 @@ export default function HomePage({ params }: PageProps) {
                   <Image
                     src="/images/manta-logo.png"
                     alt="manta"
-                    width="282"
-                    height="282"
+                    width="64"
+                    height="64"
                     className="h-4 w-4"
                   />
                   <h5 className="underline-offset-4 font-medium 	underline text-black decoration-gray-300 ">
@@ -393,8 +393,8 @@ export default function HomePage({ params }: PageProps) {
                   <Image
                     src="/images/manta-logo.png"
                     alt="manta"
-                    width="282"
-                    height="282"
+                    width="64"
+                    height="64"
                     className="h-4 w-4"
                   />
                   <h5 className="underline-offset-4	font-medium  underline text-black decoration-gray-300 ">
