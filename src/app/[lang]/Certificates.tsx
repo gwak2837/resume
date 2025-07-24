55a03268a6c0e2840c4af7fe2df87b3c2ddd88fd
Taeuk_태욱(Web) <152158725+logan2837@users.noreply.github.com>
2025-07-24 17:33:27 +0900

    feat: 디자인

---
+++ src/app/[lang]/Certificates.tsx
@@ -0,0 +1,77 @@
+export default function Certificates() {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <td>이름</td>
+          <td>내용</td>
+          <td>주관</td>
+          <td>일시</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <a
+              href="https://www.q-net.or.kr/crf005.do?id=crf00503&jmCd=1320"
+              target="_blank"
+              rel="noreferrer"
+            >
+              2025년 정기 기사 1회
+            </a>
+          </td>
+          <td className="text-center">
+            <a href="/pdf/2025-06-13_정보처리기사.pdf" target="_blank">
+              정보처리기사
+            </a>
+          </td>
+          <td className="text-center">과학기술정보통신부</td>
+          <td className="text-center">2025.06.13</td>
+        </tr>
+        <tr>
+          <td>
+            <a href="https://www.topcit.or.kr/home.do" target="_blank" rel="noreferrer">
+              TOPCIT
+            </a>{' '}
+            제15회
+          </td>
+          <td className="text-center">
+            <a href="/pdf/2021-05-22_TOPCIT_15회.pdf" target="_blank">
+              627점 (전국 4등)
+            </a>
+          </td>
+          <td className="text-center">과학기술정보통신부</td>
+          <td className="text-center">2021.05.22</td>
+        </tr>
+        <tr>
+          <td>
+            <a href="https://www.g-telp.co.kr:335/" target="_blank" rel="noreferrer">
+              G-TELP (Level 2)
+            </a>
+          </td>
+          <td className="text-center">
+            <a href="/images/2020-gtelp.webp" target="_blank">
+              82점
+            </a>
+          </td>
+          <td className="text-center">국제테스트 연구원</td>
+          <td className="text-center">2020.05.03</td>
+        </tr>
+        <tr>
+          <td>
+            <a href="https://www.hanja.ne.kr/index_original.asp" target="_blank" rel="noreferrer">
+              대한검정회
+            </a>
+          </td>
+          <td className="text-center">
+            <a href="/images/2009-hanja.webp" target="_blank">
+              준1급
+            </a>
+          </td>
+          <td className="text-center">대한민국한자교육연구회</td>
+          <td className="text-center">2009.11.28</td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
