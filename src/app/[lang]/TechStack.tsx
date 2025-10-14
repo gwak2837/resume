5a01bf6647b74ee29648811506ca17234b28910d
Taeuk_태욱(Web) <logan.gwak@ridi.com>
2025-10-14 13:17:54 +0900

    Update TechStack.tsx

--- src/app/[lang]/TechStack.tsx
+++ src/app/[lang]/TechStack.tsx
@@ -262,42 +262,42 @@ const techCategories = [
       },
     ],
   },
-  // {
-  //   title: 'AI',
-  //   icon: (
-  //     <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-  //       <path
-  //         strokeLinecap="round"
-  //         strokeLinejoin="round"
-  //         strokeWidth={1.5}
-  //         d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
-  //       />
-  //     </svg>
-  //   ),
-  //   color: 'border-rose-100',
-  //   iconColor: 'text-rose-600',
-  //   items: [
-  //     {
-  //       name: 'ComfyUI',
-  //       href: 'https://github.com/comfyanonymous/ComfyUI',
-  //       icon: null,
-  //     },
-  //     {
-  //       name: 'LoRA (Kohya SS)',
-  //       href: 'https://github.com/bmaltais/kohya_ss',
-  //       icon: null,
-  //     },
-  //   ],
-  // },
+  {
+    title: 'AI',
+    icon: (
+      <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={1.5}
+          d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
+        />
+      </svg>
+    ),
+    color: 'border-rose-100',
+    iconColor: 'text-rose-600',
+    items: [
+      {
+        name: 'ComfyUI',
+        href: 'https://github.com/comfyanonymous/ComfyUI',
+        icon: null,
+      },
+      {
+        name: 'LoRA (Kohya SS)',
+        href: 'https://github.com/bmaltais/kohya_ss',
+        icon: null,
+      },
+    ],
+  },
 ]
 
 export default function TechStack() {
   return (
-    <div className="grid gap-2">
+    <div className="grid grid-cols-2 gap-2">
       {techCategories.map((category, index) => (
         <div
           key={index}
-          className={`grid gap-2 rounded-xl bg-gradient-to-br ${category.color} border p-4`}
+          className={`flex flex-col gap-2 rounded-xl bg-gradient-to-br ${category.color} border p-4`}
         >
           {/* Header */}
           <div className="flex items-center gap-1">
